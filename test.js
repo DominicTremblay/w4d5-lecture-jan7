91c5b91a8f1d0c9f3d5a39543ef1469ac8d49d25
Dominic Tremblay <dominictremblay24@gmail.com>
2019-02-01 16:22:05 -0500

    removed settings.json from git

--- test.js
+++
@@ -1,7 +0,0 @@
-// const getUser = () => new Promise((resolve, reject) => resolve('Bob'));
-
-// getUser.then(user => console.log(user));
-
-const fct = () => new Promise((resolve, reject) => resolve('done!'));
-
-fct().then(str => console.log(str)); // done! (shows up right now)
